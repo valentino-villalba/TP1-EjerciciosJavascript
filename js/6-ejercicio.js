a49785d4b52b802b8377c704cd9ddd0be4fbd43e
Valentino Villalba <valentinovillalba61@gmail.com>
2023-10-24 02:01:56 -0300

    sexto ejercicio

---
+++ js/6-ejercicio.js
@@ -0,0 +1,8 @@
+let num1= prompt("ingrese un numero")
+let num2= prompt("ingrese otro")
+
+if (num1 > num2) {
+    document.write(num1 + " es el numero mas grande")
+} else if (num2 > num1) {
+    document.write(num2 + " es el numero mas grande")
+}
