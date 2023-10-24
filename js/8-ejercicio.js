f2546b9dbe7496c83557102674c445849bd7b333
Valentino Villalba <valentinovillalba61@gmail.com>
2023-10-24 02:02:42 -0300

    octavo ejercicio

---
+++ js/8-ejercicio.js
@@ -0,0 +1,7 @@
+let num= prompt("Escriba un numero")
+
+if (num % 2 === 0) {
+    document.write(num + " es divisible por 2")
+} else if (num % 2 === 1) {
+    document.write(num + " no es divisible por 2")
+}
